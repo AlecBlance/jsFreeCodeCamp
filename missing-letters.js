b6e99b2a710712a8d81506ab1b3c13a769584bb0
Alec Blance <17378596+AlecBlance@users.noreply.github.com>
2023-05-24 19:45:05 +0800

    Update missing-letters.js

--- missing-letters.js
+++ missing-letters.js
@@ -1,3 +1,7 @@
+// Find the missing letter in the passed letter range and return it.
+
+// If all letters are present in the range, return undefined.
+
 function fearNotLetter(str) {
   let start = str.charCodeAt(0);
   for(let i = 1; i < str.length; i++) {
