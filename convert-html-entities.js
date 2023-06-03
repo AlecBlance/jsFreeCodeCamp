e1e4b9f62591390cecdca17038748d8c2a1622c3
Alec Blance <17378596+AlecBlance@users.noreply.github.com>
2023-06-03 20:47:45 +0800

    Update convert-html-entities.js

--- convert-html-entities.js
+++ convert-html-entities.js
@@ -1,3 +1,5 @@
+// Convert the characters &, <, >, " (double quote), and ' (apostrophe), in a string to their corresponding HTML entities.
+
 function convertHTML(str) {
     const htmlEntities = {
     "&": "&amp;",
