c7453603b3a7c6bd9653d0cc827f92c706940d45
Alec Blance <17378596+AlecBlance@users.noreply.github.com>
2023-06-06 23:15:39 +0800

    Add binary agents solution

---
+++ binary-agents.js
@@ -0,0 +1,18 @@
+function binaryAgent(str) {
+  str = str.split(" ");
+  // console.log("alec",str) 
+  return str.map(binary => {
+    // console.log("alec",binary)
+    binary = binary.split("").reverse().join("");
+    let charCode = 0; 
+    for (let i = 0; i < binary.length; i++) {
+      console.log(`${parseInt(binary[i])} * ${Math.pow(2, i)}`);
+      charCode += parseInt(binary[i]) * Math.pow(2, i);
+    }
+    // console.log("char", String.fromCharCode(charCode));
+    return String.fromCharCode(charCode);
+  }).join("");
+  
+}
+
+console.log(binaryAgent("01000001 01110010 01100101 01101110 00100111 01110100 00100000 01100010 01101111 01101110 01100110 01101001 01110010 01100101 01110011 00100000 01100110 01110101 01101110 00100001 00111111"));
